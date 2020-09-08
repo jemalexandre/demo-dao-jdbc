78d16611f2bf92a9b995eeb23d105465a49bc1c2
feijuca <joseeduardo.alexandre@gmail.com>
2020-09-08 20:02:09 +0100

    All methods was implemented.

--- src/application/Program2.java
+++ src/application/Program2.java
@@ -1,5 +1,8 @@
 package application;
 
+import java.util.List;
+import java.util.Scanner;
+
 import model.dao.DaoFactory;
 import model.dao.DepartmentDao;
 import model.entities.Department;
@@ -8,16 +11,39 @@ public class Program2 {
 
 	public static void main(String[] args) {
 		
+		Scanner sc = new Scanner(System.in);
+		
 		DepartmentDao departmentDao = DaoFactory.createDepartmentDao();
 		System.out.println("=== TEST 1: department findById ===");
 		Department department = departmentDao.findById(3);
 		System.out.println(department);
 		
-		System.out.println("\n=== TEST 4: department insert ===");
+		/*System.out.println("\n=== TEST 2: department insert ===");
 		Department newDepartment = new Department(null, "IT");
 		departmentDao.insert(newDepartment);
-		System.out.println("Inserted! new id = " + newDepartment.getId());
+		System.out.println("Inserted! new id = " + newDepartment.getId());*/
+		
+		System.out.println("\n=== TEST 3: department findAll ===");
+		List<Department> list = departmentDao.findAll();
+		for (Department obj : list) {
+			System.out.println(obj);
+		}
+			
+		System.out.println("\n=== TEST 4: department update ===");
+		department = departmentDao.findById(4);
+		department.setName("Books");
+		departmentDao.update(department);
+		System.out.println("Update completed.");
 
+		
+		System.out.println("\n=== TEST 5: department delete ===");
+		System.out.print("Enter id for delete test: ");
+		int id = sc.nextInt();
+		departmentDao.deleteById(id);
+		System.out.println("Delete completed.");
+
+		sc.close();
+		
 	}
 
 }
--- src/model/dao/impl/DepartmentDaoJDBC.java
+++ src/model/dao/impl/DepartmentDaoJDBC.java
@@ -5,6 +5,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 
 import db.DB;
@@ -12,10 +13,10 @@ import db.DbException;
 import model.dao.DepartmentDao;
 import model.entities.Department;
 
-public class DepartmentDaoJDBC implements DepartmentDao{
+public class DepartmentDaoJDBC implements DepartmentDao {
 
 	private Connection conn;
-	
+
 	public DepartmentDaoJDBC(Connection conn) {
 		this.conn = conn;
 	}
@@ -24,18 +25,15 @@ public class DepartmentDaoJDBC implements DepartmentDao{
 	public void insert(Department obj) {
 		PreparedStatement st = null;
 		try {
-			st = conn.prepareStatement(
-					"INSERT INTO department "
-					+ "(Name) "
-					+ "VALUES "
-					+ "(?)", Statement.RETURN_GENERATED_KEYS);
+			st = conn.prepareStatement("INSERT INTO department " + "(Name) " + "VALUES " + "(?)",
+					Statement.RETURN_GENERATED_KEYS);
 			st.setString(1, obj.getName());
-			
+
 			int rowsAffected = st.executeUpdate();
-			
+
 			if (rowsAffected > 0) {
 				ResultSet rs = st.getGeneratedKeys();
-				if(rs.next()) {
+				if (rs.next()) {
 					int id = rs.getInt(1);
 					obj.setId(id);
 				}
@@ -52,14 +50,39 @@ public class DepartmentDaoJDBC implements DepartmentDao{
 
 	@Override
 	public void update(Department obj) {
-		// TODO Auto-generated method stub
-		
+		PreparedStatement st = null;
+		try {
+			st = conn.prepareStatement(
+					"UPDATE department "
+					+ "SET Name = ? "
+					+ "WHERE Id = ?");
+			st.setString(1, obj.getName());	
+			st.setInt(2, obj.getId());
+			
+			st.executeUpdate();
+			
+		} catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		} finally {
+			DB.closeStatement(st);
+		}
 	}
 
 	@Override
 	public void deleteById(Integer id) {
-		// TODO Auto-generated method stub
-		
+		PreparedStatement st = null;
+		try {
+			st = conn.prepareStatement("DELETE FROM department WHERE Id = ?");
+			st.setInt(1, id);
+			int rowsAffected = st.executeUpdate();
+			if(rowsAffected == 0) {
+				throw new DbException("Do not have any department with id " + id);
+			}
+		} catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		} finally {
+			DB.closeStatement(st);
+		}
 	}
 
 	@Override
@@ -67,13 +90,10 @@ public class DepartmentDaoJDBC implements DepartmentDao{
 		PreparedStatement st = null;
 		ResultSet rs = null;
 		try {
-			st = conn.prepareStatement(
-					"SELECT * " 
-					+ "FROM department "
-					+ "WHERE Id = ?");
+			st = conn.prepareStatement("SELECT * " + "FROM department " + "WHERE Id = ?");
 			st.setInt(1, id);
 			rs = st.executeQuery();
-			if(rs.next()) {
+			if (rs.next()) {
 				Department dep = instantiateDepartment(rs);
 				return dep;
 			}
@@ -86,7 +106,7 @@ public class DepartmentDaoJDBC implements DepartmentDao{
 		}
 	}
 
-	private Department instantiateDepartment(ResultSet rs) throws SQLException{
+	private Department instantiateDepartment(ResultSet rs) throws SQLException {
 		Department dep = new Department();
 		dep.setId(rs.getInt("Id"));
 		dep.setName(rs.getString("Name"));
@@ -95,8 +115,24 @@ public class DepartmentDaoJDBC implements DepartmentDao{
 
 	@Override
 	public List<Department> findAll() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+		PreparedStatement st = null;
+		ResultSet rs = null;
+		try {
+			st = conn.prepareStatement(
+					"SELECT * FROM department ORDER BY Name");
+			rs = st.executeQuery();
+			List<Department> list = new ArrayList<Department>();
+			while (rs.next()) {
+				Department dep = instantiateDepartment(rs);
+				list.add(dep);
+			}
+			return list;
+		} catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		} finally {
+			DB.closeStatement(st);
+			DB.closeResultSet(rs);
+		}
+	}	
 
 }
